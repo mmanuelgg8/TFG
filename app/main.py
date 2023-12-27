9822e4ceac297094e3fe4a4e06b3e35ed9ead285
Manuel Gonzalez <manuel.gonzalez@tupl.com>
2023-12-27 20:12:26 +0100

    tests

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import logging
 import os
-from dotenv import load_dotenv
-
-from scripts.downloader import UrlConstants, Downloader, DataTypeConstants, DataFilterConstants, ImageFormatConstants
-from utils import set_logging
+from datetime import datetime, timedelta
 
 from configuration.configuration import Configuration
+from dotenv import load_dotenv
+from scripts.downloader import DataFilterConstants, DataTypeConstants, Downloader, ImageFormatConstants, UrlConstants
+from scripts.openeoClient import OpenEOClient
+from utils import set_logging
 
 load_dotenv()
 set_logging()
@@ -14,11 +15,33 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     client_id = os.getenv("COPERNICUS_CLIENT_ID")
     client_secret = os.getenv("COPERNICUS_CLIENT_SECRET")
+    client_user = os.getenv("COPERNICUS_CLIENT_USER")
+    client_password = os.getenv("COPERNICUS_CLIENT_PASSWORD")
 
     configuration = Configuration()
     EVALSCRIPTS_PATH: str = str(configuration["evalscripts_path"])
 
     downloader = Downloader(client_id, client_secret)
+    # Define time range
+    start_date = datetime(2018, 1, 1)
+    end_date = datetime(2019, 1, 1)
+    date_interval = timedelta(weeks=1)  # Daily interval
+
+    # Create a list of data requests for each day in the time range
+    data_requests = []
+    current_date = start_date
+    while current_date <= end_date:
+        data_request = {
+            "type": DataTypeConstants.SENTINEL2_L2A.value,
+            "dataFilter": {
+                DataFilterConstants.MAX_CLOUD_COVERAGE.value: 30,
+                DataFilterConstants.MIN_CLOUD_COVERAGE.value: 0,
+                DataFilterConstants.MIN_DATE.value: current_date.strftime("%Y-%m-%d"),
+                DataFilterConstants.MAX_DATE.value: (current_date + date_interval).strftime("%Y-%m-%d"),
+            },
+        }
+        data_requests.append(data_request)
+        current_date += date_interval
     # Isla Mayor, Sevilla
     min_x, min_y = -6.215864855019264, 37.162534357525814
     max_x, max_y = -6.111682075391747, 37.10259292740977
@@ -34,6 +57,7 @@ if __name__ == "__main__":
             },
         }
     ]
+    # data = data_requests
     evalscript_path = os.path.join(EVALSCRIPTS_PATH, "evalscript.js")
     logger.info("Using evalscript: " + evalscript_path)
     with open(evalscript_path, "r") as f:
@@ -43,3 +67,7 @@ if __name__ == "__main__":
     url = UrlConstants.COPERNICUS_API_PROCESS.value
     payload = downloader.create_payload(bbox, data, ImageFormatConstants.TIFF.value, evalscript)
     downloader.download(url, payload)
+
+    # # OpenEO
+    # openeo_client = OpenEOClient("https://openeo.dataspace.copernicus.eu/openeo/1.2", client_user, client_password)
+    # download = openeo_client.download("SENTINEL2_L2A", bbox, ("2019-01-01", "2020-01-01"), ["B04", "B08"], "output.tif")
--- app/scripts/openeoClient.py
+++ app/scripts/openeoClient.py
@@ -1,7 +1,43 @@
 import logging
-import openeo as oe
+import os
 
+import openeo
+from dotenv import load_dotenv
+from openeo.rest.connection import Connection
 from utils import set_logging
 
+load_dotenv()
 set_logging()
 logger = logging.getLogger(__name__)
+
+
+class OpenEOClient:
+    def __init__(self, url, username=None, password=None):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.client_id = os.getenv("COPERNICUS_CLIENT_ID")
+        logger.info("Client id: " + str(self.client_id))
+        self.client_secret = os.getenv("COPERNICUS_CLIENT_SECRET")
+        self.connection = openeo.connect(url).authenticate_oidc_client_credentials(self.client_id, self.client_secret)
+        logger.info("Supported collection ids: " + str(self.connection.list_collection_ids()))
+
+    def connect(self):
+        self.connection = openeo.connect(self.url)
+        if self.username and self.password:
+            self.connection.authenticate_basic(self.username, self.password)
+
+    def list_collections(self):
+        return self.connection.list_collections()
+
+    def list_processes(self):
+        return self.connection.list_processes()
+
+    def download(self, collection_id, bbox, time_interval, bands, output_path):
+        collection = self.connection.load_collection(collection_id)
+        datacube = collection.filter_bbox(west=bbox[0], south=bbox[1], east=bbox[2], north=bbox[3], crs="EPSG:4326")
+        datacube = datacube.filter_temporal(start_date=time_interval[0], end_date=time_interval[1])
+        datacube = datacube.filter_bands(bands=bands)
+        result = datacube.download(format="GTiff")
+        with open(output_path, "wb") as f:
+            f.write(result)
