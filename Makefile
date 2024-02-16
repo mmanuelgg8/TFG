0c1eecebbc4587d647c91b6ee54b46a25781f8c4
Manuel <mmanuelgg@gmail.com>
2024-02-16 03:05:49 +0100

    More cases

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 # Makefile for a Python project
+IMAGE_NAME = tfg
 
 .PHONY: clean install test
 
@@ -9,10 +10,6 @@ default: install
 install:
 	pip install -r requirements.txt
 
-# Run tests
-test:
-	pytest
-
 # Clean up temporary files
 clean:
 	rm -rf __pycache__ *.pyc
@@ -20,7 +17,9 @@ clean:
 # Additional targets can be added based on project needs
 
 build-image:
-	docker build --no-cache -t tfg .
+	docker build --no-cache -t $(IMAGE_NAME) .
+	@echo y | docker container prune
+	@echo y | docker image prune
 
 run-image:
 	docker run -it -v resources:/root/resources tfg python main.py -c $(FILE)
@@ -28,53 +27,8 @@ run-image:
 run:
 	cd app && pwd && python -m main $(ARGS)
 
-download:
-	cd app && pwd && python -m main --download $(ARGS)
-
-train:
-	cd app && pwd && python -m main --train $(ARGS)
-
 test-run:
-	cd app && pwd && python -m main \
-		--bands "B04" "B08" \
-    --formula "NDVI" \
-    --bbox -6.215864855019264 37.162534357525814 -6.111682075391747 37.10259292740977 \
-    --evalscript "ndvi" \
-    --start_date "2017-01-01" \
-    --end_date "2022-01-01" \
-    --interval_type "weeks" \
-    --date_interval 1 \
-		--name_id "islamayor_ndvi" \
-		--download \
-		--train \
-		--models "random_forest" "arima" \
-		$(ARGS)
-
-test-download:
-	cd app && pwd && python -m main \
-		--bands "B04" "B08" \
-		--formula "NDVI" \
-		--bbox -6.215864855019264 37.162534357525814 -6.111682075391747 37.10259292740977 \
-		--evalscript "ndvi" \
-		--start_date "2017-01-01" \
-		--end_date "2022-01-01" \
-		--interval_type "weeks" \
-		--date_interval 1 \
-		--name_id "islamayor_ndvi" \
-		--download \
-		$(ARGS)
-
-test-train:
-	cd app && pwd && python -m main \
-		--bands "B04" "B08" \
-		--formula "NDVI" \
-		--start_date "2017-01-01" \
-		--interval_type "weeks" \
-		--date_interval 1 \
-		--name_id "islamayor_ndvi" \
-		--models "random_forest" "arima" \
-		--train \
-		$(ARGS)
+	cd app && pwd && python -m main -c "isla_mayor.json"
 
 venv:
 	virtualenv TFG
--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ def main(config_file):
 
     if config.get("download"):
         logger.info("Downloading images...")
+        logger.info("Dates: {} - {}".format(start_date, end_date))
         download(
             config["bbox"],
             config["evalscript"],
@@ -61,51 +62,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download and train models")
     parser.add_argument("--config_file", "-c", type=str, help="Name of the JSON configuration file")
     args = parser.parse_args()
-    path = os.path.join(os.path.dirname(__file__), "../resources/properties/" + args.config_file)
+    logger.info("Using config file: " + args.config_file)
+    path = os.path.join(os.path.dirname(__file__), "..", "resources", "properties", args.config_file)
     main(path)
-
-# def main():
-#     available_models = ["arima", "random_forest"]
-#     parser = argparse.ArgumentParser(description="Download and train models")
-#     parser.add_argument("--bbox", type=float, nargs=4, help="Bounding box coordinates")
-#     parser.add_argument("--evalscript", type=str, help="Evalscript")
-#     parser.add_argument("--start_date", type=lambda s: datetime.strptime(s, "%Y-%m-%d"), help="Start date")
-#     parser.add_argument("--end_date", type=lambda s: datetime.strptime(s, "%Y-%m-%d"), help="End date")
-#     parser.add_argument("--date_interval", type=int, help="Date interval")
-#     parser.add_argument("--interval_type", choices=["months", "weeks"], default="weeks", help="Interval type")
-#     parser.add_argument("--name_id", type=str, help="Name id")
-#     parser.add_argument("--bands", type=str, nargs="+", help="Bands")
-#     parser.add_argument("--formula", type=str, help="Formula")
-#     parser.add_argument("--kpi", choices=["mean", "max", "min", "std"], default="mean", help="KPI")
-#     parser.add_argument("--download", action="store_true", help="Download")
-#     parser.add_argument("--train", action="store_true", help="Train")
-#     parser.add_argument("--models", choices=available_models, nargs="+", help="Models to train")
-#     args = parser.parse_args()
-#
-#     if args.interval_type == "months":
-#         args.date_interval = relativedelta(months=args.date_interval)
-#     elif args.interval_type == "weeks":
-#         args.date_interval = relativedelta(weeks=args.date_interval)
-#
-#     if args.download:
-#         logger.info("Downloading images...")
-#         download(args.bbox, args.evalscript, args.start_date, args.end_date, args.date_interval, args.name_id)
-#
-#     if args.train:
-#         logger.info("Training models...")
-#         process_data = ProcessData(args.name_id, args.date_interval, args.start_date, args.bands, args.formula)
-#         df = process_data.create_dataframe(args.kpi)
-#         logger.info("Dataframe: \n{}".format(df))
-#         models = []
-#         for model in args.models:
-#             if model == "arima":
-#                 models.append(ArimaModel(df, args.kpi))
-#             elif model == "random_forest":
-#                 models.append(RandomForestModel(df, args.kpi))
-#         for model in models:
-#             model.train_model()
-#             model.evaluate()
-#
-#
-# if __name__ == "__main__":
-#     main()
